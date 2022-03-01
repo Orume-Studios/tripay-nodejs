96548c75e109d8d9645050296449e88173fde58c
NotYusta <xabhista19@gmail.com>
2022-03-01 23:00:39 +0700

    feat: update package.json

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 const { TriPay: test} = require('../index');
-
-const tripay = new test(credentials);
+const config = require('./.config');
+const tripay = new test(config.tripayCredentials);
 
 tripay.getClosedPaymentAPI().getTransaction("agagas").then(result => {
     console.log(result)
